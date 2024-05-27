0554e7288f83e6b2e3649efbb9931997391eb619
AIWASS23 <marcelodearaujo677@gmail.com>
2024-05-27 10:04:21 -0300

    📊 Implementando trabalho 3

--- trabalho3.py
+++ trabalho3.py
@@ -1,97 +1,8 @@
-# import numpy as np
-# from sklearn.decomposition import PCA
-# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-# from skimage.feature import hog
-# from sklearn.model_selection import train_test_split, cross_val_score, KFold
-# from sklearn.svm import SVC
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import confusion_matrix
-# from keras.models import Sequential
-# from keras.layers import Dense, Flatten, Input
-# from keras.utils import to_categorical
-
-# # Carregamento dos dados
-# data = np.loadtxt('ocr_car_numbers_rotulado.txt', delimiter=' ')
-# X = data[:, :-1].reshape(-1, 35, 35)  # Atributos (imagens)
-# y = data[:, -1]  # Rótulos
-
-# # Função para extrair HOG
-# def extract_hog(images):
-#     hog_features = []
-#     for img in images:
-#         feature = hog(img, pixels_per_cell=(4, 4), cells_per_block=(2, 2), feature_vector=True)
-#         hog_features.append(feature)
-#     return np.array(hog_features)
-
-# # Extração de Atributos
-# X_pca = PCA(n_components=50).fit_transform(X.reshape(len(X), -1))
-# X_lda = LDA(n_components=9).fit_transform(X.reshape(len(X), -1), y)
-# X_hog = extract_hog(X)
-
-# # Divisão em treino e teste
-# X_train_pca, X_test_pca, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=42)
-# X_train_lda, X_test_lda = train_test_split(X_lda, test_size=0.2, random_state=42)
-# X_train_hog, X_test_hog = train_test_split(X_hog, test_size=0.2, random_state=42)
-
-# # Modelos
-# classifiers = [
-#     ('SVM', SVC()),
-#     ('Random Forest', RandomForestClassifier())
-# ]
-
-# # Modelo de Deep Learning
-# def create_nn(input_shape):
-#     model = Sequential()
-#     model.add(Input(shape=input_shape))
-#     model.add(Flatten())
-#     model.add(Dense(128, activation='relu'))
-#     model.add(Dense(10, activation='softmax'))
-#     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-#     return model
-
-# # Avaliação
-# results = {}
-# extractors = {
-#     'PCA': (X_train_pca, X_test_pca),
-#     'LDA': (X_train_lda, X_test_lda),
-#     'HOG': (X_train_hog, X_test_hog)
-# }
-
-# for extractor_name, (X_train, X_test) in extractors.items():
-#     results[extractor_name] = {}
-#     for clf_name, clf in classifiers:
-#         kfold = KFold(n_splits=10, shuffle=True, random_state=42)
-#         cv_results = cross_val_score(clf, X_train, y_train, cv=kfold, scoring='accuracy')
-#         results[extractor_name][clf_name] = {
-#             'Accuracy Mean': cv_results.mean(),
-#             'Accuracy Std': cv_results.std()
-#         }
-#         clf.fit(X_train, y_train)
-#         y_pred = clf.predict(X_test)
-#         results[extractor_name][clf_name]['Confusion Matrix'] = confusion_matrix(y_test, y_pred)
-    
-#     # Deep Learning
-#     model = create_nn(X_train.shape[1:])
-#     model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=0)
-#     y_pred_nn = np.argmax(model.predict(X_test), axis=1)
-#     results[extractor_name]['Neural Network'] = {
-#         'Confusion Matrix': confusion_matrix(y_test, y_pred_nn)
-#     }
-
-# # Exibindo resultados
-# for extractor_name, classifiers in results.items():
-#     print(f"\nExtractor: {extractor_name}")
-#     for clf_name, result in classifiers.items():
-#         print(f"{clf_name}:")
-#         if 'Accuracy Mean' in result:
-#             print(f"\tAccuracy Mean: {result['Accuracy Mean']}")
-#             print(f"\tAccuracy Std: {result['Accuracy Std']}")
-#         print(f"\tConfusion Matrix:\n{result['Confusion Matrix']}\n")
-
 import numpy as np
 from sklearn.decomposition import NMF, PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from skimage.feature import hog
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
@@ -105,7 +16,7 @@ from tensorflow.keras.utils import to_categorical
 
 # Carregamento dos dados
 data = np.loadtxt('ocr_car_numbers_rotulado.txt', delimiter=' ')
-X = data[:, :-1].reshape(-1, 35, 35)  # Atributos (imagens)
+X = data[:, :-1].reshape(-1, 35, 35)  # Atributos
 y = data[:, -1]  # Rótulos
 
 # Extração de atributos
@@ -113,7 +24,7 @@ y = data[:, -1]  # Rótulos
 def extract_hog(images):
     hog_features = []
     for img in images:
-        feature = hog(img, pixels_per_cell=(4, 4), cells_per_block=(2, 2), feature_vector=True)
+        feature = hog(img, pixels_per_cell = (4, 4), cells_per_block = (2, 2), feature_vector = True)
         hog_features.append(feature)
     return np.array(hog_features)
 
@@ -123,16 +34,12 @@ X_lda = LDA(n_components=9).fit_transform(X.reshape(len(X), -1), y)
 X_nmf = NMF(n_components=50, max_iter=1000).fit_transform(X.reshape(len(X), -1))
 X_hog = extract_hog(X)
 
-# --- Espaco dos testes --- #
 
 # Divisão em treino e teste
-X_train_pca, X_test_pca, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=42)
-X_train_lda, X_test_lda = train_test_split(X_lda, test_size=0.2, random_state=42)
-X_train_hog, X_test_hog = train_test_split(X_hog, test_size=0.2, random_state=42)
-X_train_nmf, X_test_nmf = train_test_split(X_nmf, test_size=0.2, random_state=42)
-
-# --- Espaco dos testes --- #
-
+X_train_pca, X_test_pca, y_train, y_test = train_test_split(X_pca, y, test_size = 0.2, random_state = 42)
+X_train_lda, X_test_lda = train_test_split(X_lda, test_size = 0.2, random_state = 42)
+X_train_hog, X_test_hog = train_test_split(X_hog, test_size = 0.2, random_state = 42)
+X_train_nmf, X_test_nmf = train_test_split(X_nmf, test_size = 0.2, random_state = 42)
 
 # Modelos
 classifiers = [
@@ -140,8 +47,8 @@ classifiers = [
     ('Random Forest', RandomForestClassifier()),
     ('Decision Tree', DecisionTreeClassifier()),
     ('Naive Bayes Gaussian', GaussianNB()),
-    # --- Espaco dos testes --- #
-    # ('Gradient Boosting Machines', GradientBoostingClassifier()),
+    ('Gradient Boosting Machines', GradientBoostingClassifier()),
+    
     # #('MLP', MLPClassifier())
     # ('MLP', MLPClassifier(hidden_layer_sizes=(100,), max_iter=500))
 ]
@@ -149,11 +56,11 @@ classifiers = [
 # Função para criar modelo de Deep Learning
 def create_nn(input_shape):
     model = Sequential()
-    model.add(Input(shape=input_shape))
+    model.add(Input(shape = input_shape))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.add(Dense(128, activation = 'relu'))
+    model.add(Dense(10, activation = 'softmax'))
+    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
     return model
 
 # Classe para integrar modelo Keras com scikit-learn
